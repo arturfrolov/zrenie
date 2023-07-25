e9cc74c9a8c5bcbabf9e5673e8d9fc48de7d92f7
Artur Frolov <artur.frolov@forforce.com>
2023-07-25 14:43:09 +0300

    ch

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,5 @@
 import tabs from './modules/tabs';
 import calc from './modules/calc';
-import cards from './modules/cards';
 import forms from './modules/forms';
 import modal from './modules/modal';
 import slider from './modules/slider';
