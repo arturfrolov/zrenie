40b17e8147aa005f783148d6577ef76afb019355
Artur <djajikamail@gmail.com>
2023-07-30 23:17:48 +0300

    add modal and hamburger menu

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,4 @@
+import 'bootstrap';
 import '../index.html';
 import '../sass/style.scss';
 
@@ -9,3 +10,25 @@ import '../sass/style.scss';
 // import timer from './modules/timer';
 // import { openModal } from './modules/modal';
 // import bootstrap from 'bootstrap';
+
+const hamburger = document.querySelector('.hamburger');
+const menu = document.querySelector('.menu');
+const close = menu.querySelector('.menu__close');
+
+hamburger.addEventListener('click', () => {
+  menu.classList.add('active');
+});
+
+close.addEventListener('click', () => {
+  menu.classList.remove('active');
+});
+
+// window.addEventListener('resize', () => {
+//   let viewport_width = Math.max(
+//     document.documentElement.clientWidth,
+//     window.innerWidth || 0
+//   );
+//   if (viewport_width < 992) {
+//     menu.innerHTML = ``;
+//   }
+// });
