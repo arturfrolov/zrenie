cf6ca0467aea4727115834faa9d5cd677fb7b0a7
Artur Frolov <artur.frolov@forforce.com>
2023-07-31 10:36:31 +0300

    47674 Стажировка
    https://rm.forforce.com/issues/47674

--- src/js/main.js
+++ src/js/main.js
@@ -2,18 +2,12 @@ import 'bootstrap';
 import '../index.html';
 import '../sass/style.scss';
 
-// import tabs from './modules/tabs';
-// import calc from './modules/calc';
-// import forms from './modules/forms';
 // import modal from './modules/modal';
-// import slider from './modules/slider';
-// import timer from './modules/timer';
-// import { openModal } from './modules/modal';
-// import bootstrap from 'bootstrap';
 
 const hamburger = document.querySelector('.hamburger');
 const menu = document.querySelector('.menu');
 const close = menu.querySelector('.menu__close');
+const container = menu.querySelector('.container');
 
 hamburger.addEventListener('click', () => {
   menu.classList.add('active');
@@ -23,12 +17,14 @@ close.addEventListener('click', () => {
   menu.classList.remove('active');
 });
 
-// window.addEventListener('resize', () => {
-//   let viewport_width = Math.max(
-//     document.documentElement.clientWidth,
-//     window.innerWidth || 0
-//   );
-//   if (viewport_width < 992) {
-//     menu.innerHTML = ``;
-//   }
-// });
+window.addEventListener('resize', () => {
+  let viewport_width = Math.max(
+    document.documentElement.clientWidth,
+    window.innerWidth || 0
+  );
+  if (viewport_width < 992) {
+    container.classList.add('p-0');
+  } else if (viewport_width >= 992) {
+    container.classList.remove('p-0');
+  }
+});
