e699d818774a7856cb2c3ab263dcf8281e7f0210
Artur <djajikamail@gmail.com>
2023-08-02 21:30:48 +0300

    small changes

--- src/js/modules/tabs.js
+++ src/js/modules/tabs.js
@@ -1,42 +1,47 @@
-function tabs(tabsSelector, tabsContentSelector, tabsParentSelector, activeClass) {
-    // Tabs
+function tabs(
+  tabsSelector,
+  tabsContentSelector,
+  tabsParentSelector,
+  activeClass
+) {
+  // Tabs
 
-    const tabs = document.querySelectorAll(tabsSelector);
-    const tabsContent = document.querySelectorAll(tabsContentSelector);
-    const tabsParent = document.querySelector(tabsParentSelector);
+  const tabs = document.querySelectorAll(tabsSelector);
+  const tabsContent = document.querySelectorAll(tabsContentSelector);
+  const tabsParent = document.querySelector(tabsParentSelector);
 
-    function hideTabContent() {
-        tabsContent.forEach(tabItem => {
-            tabItem.style.display = 'none';
-            tabItem.classList.remove('fade');
-        });
-
-        tabs.forEach(tab => {
-            tab.classList.remove(activeClass);
-        });
-    }
-
-    function showTabContent(i = 0) {
-        tabsContent[i].style.display = 'block';
-        tabsContent[i].classList.add('fade');
-        tabs[i].classList.add('tabheader__item_active');
-    }
-
-    tabsParent.addEventListener('click', (event) => {
-        const target = event.target;
-
-        if (target && target.classList.contains(tabsSelector.slice(1))) {
-            tabs.forEach((tab, index) => {
-                if (target == tab) {
-                    hideTabContent();
-                    showTabContent(index);
-                }
-            });
-        }
+  function hideTabContent() {
+    tabsContent.forEach((tabItem) => {
+      tabItem.style.display = 'none';
+      tabItem.classList.remove('fade');
     });
 
-    hideTabContent();
-    showTabContent();
+    tabs.forEach((tab) => {
+      tab.classList.remove(activeClass);
+    });
+  }
+
+  function showTabContent(i = 0) {
+    tabsContent[i].style.display = 'block';
+    tabsContent[i].classList.add('fade');
+    tabs[i].classList.add('tabheader__item_active');
+  }
+
+  tabsParent.addEventListener('click', (event) => {
+    const { target } = event;
+
+    if (target && target.classList.contains(tabsSelector.slice(1))) {
+      tabs.forEach((tab, index) => {
+        if (target === tab) {
+          hideTabContent();
+          showTabContent(index);
+        }
+      });
+    }
+  });
+
+  hideTabContent();
+  showTabContent();
 }
 
 export default tabs;
