4d0b1a82fd0663c3831595cd0979ad495cd109d1
Artur Frolov <artur.frolov@forforce.com>
2023-08-09 10:57:16 +0300

    47674 Стажировка
    https://rm.forforce.com/issues/47674

--- src/js/modules/search.js
+++ src/js/modules/search.js
@@ -1,12 +1,17 @@
 function search() {
-  const searchBtn = document.querySelector('.js-search');
+  const searchBtnList = document.querySelectorAll('.js-search');
   const wrap = document.querySelector('.wrap');
   const input = document.querySelector('.input');
+  const searchText = document.querySelector('.header__search_text');
 
-  searchBtn.addEventListener('click', () => {
-    wrap.classList.toggle('active');
-    input.classList.toggle('active');
-    input.focus();
+  searchBtnList.forEach((searchBtn) => {
+    searchBtn.addEventListener('click', () => {
+      wrap.classList.toggle('active');
+      input.classList.toggle('active');
+      searchText.classList.toggle('hide');
+      input.closest('form').reset();
+      input.focus();
+    });
   });
 }
 
