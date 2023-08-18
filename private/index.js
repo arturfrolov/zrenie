3245bebf828e8b576baf34be8180e1249ca55191
Artur Frolov <artur.frolov@forforce.com>
2023-08-18 17:06:13 +0300

    47674 Стажировка
    https://rm.forforce.com/issues/47674

--- private/index.js
+++ private/index.js
@@ -2,7 +2,7 @@ import 'bootstrap';
 import resize from './components/resize/resize';
 import hamburgerMenu from './blocks/header/hamburger-menu';
 import fixedmenu from './blocks/header/fixedmenu';
-import search from './components/search/search';
+import search from './blocks/header/search';
 import forms from './components/modal/modal';
 import pageUp from './components/pageup/pageup';
 
