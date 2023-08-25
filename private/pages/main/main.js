5fee99d51daf07a431a2a69751bf445b6a1d30cc
Artur Frolov <artur.frolov@forforce.com>
2023-08-25 15:21:46 +0300

    47674 Стажировка
    https://rm.forforce.com/issues/47674

---
+++ private/pages/main/main.js
@@ -0,0 +1,8 @@
+// import 'bootstrap';
+// import forms from '../../components/modal/modal';
+// import pageUp from '../../blocks/read-more/pageup';
+// import header from '../../blocks/header/header';
+//
+// header();
+// forms('form', '.modal', '.modal-close');
+// pageUp();
